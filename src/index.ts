d5330d72aea406994937698f613a9384991c8f03
AugustoVultaggio <98132241+AugustoVultaggio@users.noreply.github.com>
2022-08-18 19:44:12 -0300

    Puse la foto de Cucho

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { Application, Sprite } from 'pixi.js'
+import { Application, Loader, Sprite } from 'pixi.js'
 
 const app = new Application({
 	view: document.getElementById("pixi-canvas") as HTMLCanvasElement,
@@ -9,9 +9,14 @@ const app = new Application({
 	height: 480
 });
 
-const clampy: Sprite = Sprite.from("clampy.png");
+Loader.shared.add({url: "./Cucho.png", name: "myCucho"});
 
-clampy.anchor.set(0.5);
+
+const clampy: Sprite = Sprite.from("./Cucho.png");
+
+console.log("Hola mundo",clampy.width, clampy.height);
+
+// clampy.anchor.set(0.5);
 
 clampy.x = 300
 clampy.y = 300
